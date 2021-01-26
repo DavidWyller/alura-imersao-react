19feda918ec5ebc77fe6b9e39d3cd7d35b1a09a6
David Wyller <wyller.dg@gmail.com>
2021-01-26 16:40:35 -0300

    Final da aula2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import styled from 'styled-components';
+import Head from 'next/head';
+import React from 'react';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
 
 // const Title = styled.h1`
 //   font-size: 50px;
@@ -35,25 +40,49 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz - David</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>
-              QUIZ do David v4
+              QUIZ - David v4
             </h1>
           </Widget.Header>
           <Widget.Content>
-            
-            <p>
-              Lorem ipsum dolor sit amet..
-            </p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // console.log('Fazendo uma submissão por meio do React');
+              // router manda para a aproxima página
+            }}
+            >
+              <input
+                placeholder="Digite o seu nome"
+                onChange={function (infosDoEvento) {
+                  // console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Vamos começar,&nbsp;
+                {name}
+                !
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
+
         <Widget>
           <Widget.Content>
             <h1>
@@ -64,12 +93,9 @@ export default function Home() {
             </p>
           </Widget.Content>
         </Widget>
-        <Footer>
-
-        </Footer>
-     </QuizContainer>
-     <GitHubCorner projectUrl="https://github.com/DavidWyller"/>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/DavidWyller" />
     </QuizBackground>
-    
   );
 }
